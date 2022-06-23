b258e0a8263b46387d2baab4107212f2c9d7ec14
gitanjali1405 <gitanjalijadhav1998@gmail.com>
2022-06-23 12:54:37 +0530

    resource updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,17 +8,43 @@ provider "google" {
   zone = var.zone
 }
 
-resource "google_sql_database_instance" "master" {
-  name = "master1"
+resource "google_sql_database_instance" "cdc-poc" {
+  name = "cdc-poc-sqlserver"
   database_version = "SQLSERVER_2019_STANDARD"
-  region       = var.region
+  region = var.region
 
   settings {
     tier = "db-f1-micro"
+      ip_configuration {
+        ipv4_enabled    = false
+        //private_network = google_compute_network.private_network.id
+      }
   }
 }
 
 resource "google_composer_environment" "test" {
-  name   = "example-composer-env"
+  name   = "cdc-poc-composer-env"
   region = var.region
+  disk_size_gb = 30
+  tags = ["cdc-poc"]
+  config {
+    node_config {
+      zone         = "us-central1-a"
+      machine_type = "n1-standard-1"
+
+      //network    = google_compute_network.private_network.id
+      //subnetwork = google_compute_subnetwork.private_network.id
+      service_account = google_service_account.cdc.name
+    }
+    database_config {
+      machine_type = "db-n1-standard-2"
+    }
+    web_server_config {
+      machine_type = "composer-n1-webserver-2"
+    }
+  }
+}
+
+resource "google_service_account" "cdc" {
+account_id   = " cdc-service-account-1@di-gcp-351221.iam.gserviceaccount.com"
 }
