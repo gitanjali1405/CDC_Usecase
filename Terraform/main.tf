79ad958c7850bd4dfd3044453b3162b5a004a9ae
gitanjali1405 <gitanjalijadhav1998@gmail.com>
2022-07-01 10:05:43 +0530

    resource updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,17 +4,9 @@ provider "google" {
   zone = var.zone
 }
 
-
 resource "google_sql_database_instance" "instance" {
   name = "cdc-poc-sqlserver"
   database_version = "SQLSERVER_2019_STANDARD"
   region = var.region
 
-  settings {
-    tier = "db-f1-micro"
-      ip_configuration {
-        ipv4_enabled    = false
-        //private_network = google_compute_network.private_network.id
-      }
-  }
 }
