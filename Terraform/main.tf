c9c4d58df687161a3b5b438ab4c9f33dd6b1ccd6
gitanjali1405 <gitanjalijadhav1998@gmail.com>
2022-07-01 10:40:26 +0530

    resource updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,16 @@
 provider "google" {
-  project = var.project_name
-  region = var.region
+  project = "${var.project_name}"
+  region = "${var.region}"
   zone = var.zone
 }
 
 resource "google_sql_database_instance" "instance" {
   name = "cdc-poc-sqlserver"
   database_version = "SQLSERVER_2019_STANDARD"
-  region = var.region
+  region = "${var.region}"
+
+  settings {
+    tier = "db-f1-micro"
+  }
 
 }
